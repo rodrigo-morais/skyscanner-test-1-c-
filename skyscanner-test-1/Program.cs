a81d7b8508e1cb8f820a0262a312f0243d562338
Rodrigo Morais <morais.rm@gmail.com>
2015-06-20 06:32:35 -0300

    Test 1 in C#.

---
+++ skyscanner-test-1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace skyscanner_test_1
+{
+    class Program
+    {
+        private static int solution(int n)
+        {
+            if (n <= 1)
+            {
+                return n;
+            }
+            else
+            {
+                return solution(n - 1) + int.Parse(Math.Pow(double.Parse(n.ToString()),2.0).ToString());
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number:");
+
+            var n = 0;
+            int.TryParse(Console.ReadLine(), out n);
+
+            if(n == 0){
+                Console.WriteLine("Not a number");
+                Console.ReadLine();
+            }
+            else{
+                Console.WriteLine("The solution is: " + solution(n).ToString());
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
